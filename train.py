0703ebcb8d8cb90d087d2ba3ab46eeb602fd7fc6
Moritz <email@moritz-huebner.de>
2019-03-08 10:07:15 +1100

    Some changes

--- train.py
+++ train.py
@@ -7,11 +7,12 @@
 # Import modules
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 # from keras.layers import LSTM
 from keras.layers import Activation
 from keras.utils import plot_model
 from utilities import *
+from scipy import signal
 
 # Create training arrays
 # In this demonstration I create our numpy arrays and then
@@ -27,22 +28,37 @@ Y_train = np.empty(N_train)
 # Load the data from file
 for x in range(N_train):
     # This will create x = 0, 1, 2...to N_train-1
-    X_train[x,], Y_train[x] = read_training_data(x + 1, N_sequence)
+    X_train[x, ], Y_train[x] = read_training_data(x + 1, N_sequence)
 
 # Also create the numpy arrays for the testing data set
 N_test = 50
 X_test = np.empty([N_test, N_sequence])
 Y_test = np.empty(N_test)
 for x in range(N_test):
-    X_test[x,], Y_test[x] = read_test_data(x + 1, N_sequence)
+    X_test[x, ], Y_test[x] = read_test_data(x + 1, N_sequence)
+    b, a = signal.butter(2, 0.4)
+    zi = signal.lfilter_zi(b, a)
+    z, _ = signal.lfilter(b, a, X_test[x, ], zi=zi * sig[0])
+    sig = signal.filtfilt(b, a, X_test[x, ])
 
-# Create our Keras model - an RNN (in Keras this is a Sequence)
+for i in range(len(X_train)):
+    X_train[i] = apply_noise_and_filter(X_train[i])
+
+for i in range(len(X_test)):
+    X_test[i] = apply_noise_and_filter(X_test[i])
+
+
 model = Sequential()
 
 # Configure our RNN by adding neural layers with activation functions
+model.add(Dropout(0.3, input_shape=(N_sequence, )))
 model.add(Dense(16, activation='relu', input_dim=N_sequence))
 model.add(Dense(8, activation='tanh'))
+# model.add(Dropout(0.2))
+# model.add(Dense(8, activation='tanh'))
+# model.add(Dropout(0.4))
 model.add(Dense(1, activation='sigmoid'))
+
 
 # The commented lines below are designed to offer some insight
 # into the use of layers without activation functions.
