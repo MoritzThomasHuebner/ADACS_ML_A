195010df9dbe3ee0b3fd59f3dad23456282e907a
Moritz <email@moritz-huebner.de>
2019-03-07 11:52:35 +1100

    PEP8

--- train.py
+++ train.py
@@ -8,7 +8,7 @@
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
-#from keras.layers import LSTM
+# from keras.layers import LSTM
 from keras.layers import Activation
 from keras.utils import plot_model
 from utilities import *
@@ -16,9 +16,9 @@ from utilities import *
 # Create training arrays
 # In this demonstration I create our numpy arrays and then
 # load each time sequence in one-by-one.
-N_train = 200        # Number of elements to train
-N_sequence = 128     # Length of each piece of data
-N_epochs = 300	     # Number of epochs
+N_train = 200  # Number of elements to train
+N_sequence = 128  # Length of each piece of data
+N_epochs = 300  # Number of epochs
 
 # Create the training sequence data (X) and each set's classification (Y).
 X_train = np.empty([N_train, N_sequence])
@@ -26,21 +26,21 @@ Y_train = np.empty(N_train)
 
 # Load the data from file
 for x in range(N_train):
-	# This will create x = 0, 1, 2...to N_train-1
-	X_train[x,], Y_train[x] = read_training_data(x+1, N_sequence)
+    # This will create x = 0, 1, 2...to N_train-1
+    X_train[x,], Y_train[x] = read_training_data(x + 1, N_sequence)
 
 # Also create the numpy arrays for the testing data set
 N_test = 50
 X_test = np.empty([N_test, N_sequence])
 Y_test = np.empty(N_test)
 for x in range(N_test):
-        X_test[x,], Y_test[x] = read_test_data(x+1, N_sequence)
+    X_test[x,], Y_test[x] = read_test_data(x + 1, N_sequence)
 
 # Create our Keras model - an RNN (in Keras this is a Sequence)
 model = Sequential()
 
 # Configure our RNN by adding neural layers with activation functions
-model.add(Dense(16, activation='relu',input_dim=N_sequence))
+model.add(Dense(16, activation='relu', input_dim=N_sequence))
 model.add(Dense(8, activation='tanh'))
 model.add(Dense(1, activation='sigmoid'))
 
@@ -48,8 +48,8 @@ model.add(Dense(1, activation='sigmoid'))
 # into the use of layers without activation functions.
 # The 3 lines above should be commented out before uncommenting these.
 # No activation layers
-#model.add(Dense(16,input_dim=N_sequence))
-#model.add(Dense(1))
+# model.add(Dense(16,input_dim=N_sequence))
+# model.add(Dense(1))
 
 # Compile model and print summary
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
@@ -64,5 +64,4 @@ plot_history(history)
 # Final evaluation of the model using the Test Data
 print("Evaluating Test Set")
 scores = model.evaluate(X_test, Y_test, verbose=1)
-print("Accuracy: %.2f%%" % (scores[1]*100))
-
+print("Accuracy: %.2f%%" % (scores[1] * 100))
